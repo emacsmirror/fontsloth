10e06cdc7d6744b239de706085c04af2c16ba706
Fran Ley <fran.ley@mailfence.com>
2025-06-05 11:45:18 -0700

    ; 0.17.1 -> 0.17.2
    Comments only change to bump version

--- fontsloth-raster.el
+++ fontsloth-raster.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021 Jo Gay <jo.gay@mailfence.com>
 
 ;; Author: Jo Gay <jo.gay@mailfence.com>
-;; Version: 0.17.1
+;; Version: 0.17.2
 ;; Homepage: https://github.com/jollm/fontsloth
 ;; Keywords: data, font, rasterization, ttf, otf
 
